d95dbac48b0dd5a6b52aaff24f834d4dfc228a55
Orrv2904 <Omarrv2904@gmail.com>
2023-03-18 15:02:46 -0600

    Agregando funcion de Register y Login :construction:

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, session, render_template, request, flash, redirect, url_for
 from flask_session import Session
+from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 from dotenv import load_dotenv
@@ -26,7 +27,65 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.route("/")
-def index():
-    return "Project 1: TODO"
+def log():
+    #return "Project 1: TODO"
+    return render_template("auth.html")
+
+@app.route("/books", methods=['GET','POST'])
+def books():
+    if request.methods == 'GET':
+        return render_template("index.html")
+
+
+@app.route('/Auth')
+def Auth():  
+    return render_template("auth.html")
+
+@app.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        rname = request.form.get("name")
+        remail = request.form.get("email")
+        rpassword = request.form.get("password")
+        hashed_password = generate_password_hash(rpassword)
+
+        if not rname or not remail or not rpassword:
+            flash("Por favor ingrese todos los campos", "info")
+            return render_template("/Auth")
+
+        try:
+            verificar_usuario = text("SELECT EXISTS(SELECT 1 FROM users WHERE name = :rname)")
+            verificar_correo = text("SELECT EXISTS(SELECT 1 FROM users WHERE email = :remail)")
+            res2 = db.execute(verificar_usuario, {"rname" :rname})
+            res3 = db.execute(verificar_correo, {"remail" :remail})
+            user_exists = res2.fetchone()[0]
+            email_exists = res3.fetchone()[0]
+
+            if user_exists or email_exists:
+                flash("El usuario o el correo electrónico ya existe", "error")
+                return render_template("/Auth")
+            
+            agregar_usuario = text("INSERT INTO users (name, email, password) VALUES (:rname, :remail, :hashed_password)")
+            db.execute(agregar_usuario, {"rname" :rname, "remail" :remail, "hashed_password" :hashed_password})
+            db.commit()
+            db.close()
+            flash("Usuario registrado exitosamente", "success")
+            return redirect('/Auth')
+        except Exception as e:
+            db.rollback()
+            print("Error: ", str(e))
+            #flash("Ha ocurrido un error", "error")
+            abort(404)
