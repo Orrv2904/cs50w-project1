bd340c535ed4662e96e01f06dd7dded924752d41
Orrv2904 <Omarrv2904@gmail.com>
2023-03-26 21:22:15 -0600

    Creando tabla review

--- tables.py
+++ tables.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class Books(db.Model):
     __tablename__ = "books"
     id = db.Column(db.Integer, primary_key=True)
-    isbn = db.Column(db.String, nullable=False)
+    isbn = db.Column(db.String, nullable=False, unique=True)
     title = db.Column(db.String, nullable=False)
     author = db.Column(db.String, nullable=False)
     year = db.Column(db.String, nullable=False)
@@ -17,3 +17,11 @@ class Users(db.Model):
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
+
+class Review(db.Model):
+    __tablename__ = "review"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    isbn = db.Column(db.String, db.ForeignKey("books.isbn"), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.String)
