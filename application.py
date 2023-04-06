38324b02518aae813e4c62ec9b3262468441da0c
Orrv2904 <Omarrv2904@gmail.com>
2023-04-06 16:23:36 -0600

    tiempo de respuesta del autocompletado agregado

--- application.py
+++ application.py
@@ -26,7 +26,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv("DATABASE_URL"), pool_size=20, max_overflow=30)
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.after_request
